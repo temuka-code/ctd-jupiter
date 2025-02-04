91a0d853a09dda2b85e6ef6921f1747886a8bee0
Budkas <budkas@Budkass-MacBook-Pro.local>
2025-02-04 01:03:24 -0700

    added functionality to message, tried to fix about section, failed

--- js/index.js
+++ js/index.js
@@ -22,43 +22,54 @@ for (let skill of skillsList) {
 }
 
 //Handle events for message form 
-let messageForms = document.getElementsByName("leave_message")
+let messageForms = document.getElementById("messageForm")
 console.log(messageForms);
 let messageForm = messageForms[0];
 
-let messageSection = document.getElementById("message-section");
-messageSection.hidden = true;
+let messageSection = document.getElementById("message_section");
+messageSection.hidden = true; 
 
-const messageList = messageSection.querySelector("ul");
+let messageList = messageSection.getElementsByTagName("ul");
 console.log(messageList);
 messageSection.hidden=true;
 
-messageForm.addEventListener("submit",  (event)=> {
+messageForms.addEventListener("submit",(event)=>{
     
     event.preventDefault();
     let name = event.target.usersName.value;
     let email = event.target.usersEmail.value;
     let message = event.target.usersMessage.value;
-    // console.log(userName);
+    // console.log(name);
     // console.log(email);
-    // console.log(usersMessage);
+    // console.log(message);
 
-    //Create new message list item
+    //Creating new message list item
     let newMessage = document.createElement("li");
     newMessage.innerHTML = `<a href="mailto:${email}">${name}</a><span>${message}</span>`;
 
-    //Create and append the removeButton
-    let removeButton = createRemoveButton();
-    newMessage.appendChild(removeButton);
+    // removeButton
+    const deleteButton = createDeleteButton();
+    newMessage.appendChild(deleteButton);
 
 
-    //show the message section if it was hidden
+    
     messageSection.hidden = false;
 
-    //Append the newMessage to the list
+    //append 
     messageList[0].appendChild(newMessage);
-
-    //reset the form
+        
     event.target.reset();
 
 });
+function createDeleteButton() {
+    let deleteButton = document.createElement("button");
+    deleteButton.innerText = "Delete";
+    deleteButton.setAttribute("type", "button");
+    deleteButton.addEventListener("click", function() {
+        const entry = deleteButton.parentNode;
+        entry.remove();
+
+    });
+    return deleteButton;
+    
+};
